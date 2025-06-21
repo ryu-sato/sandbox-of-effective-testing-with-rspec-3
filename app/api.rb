6a665a1f9c43ca8f4eceb3a425369a2b4bf81f41
Ryu Sato <ryu@weseek.co.jp>
2025-06-21 10:07:22 +0000

    ledger classを追加

--- app/api.rb
+++ app/api.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
 require 'json'
 
+class Ledger
+  def record(_expense)
+    ExpenseTracker::RecordResult.new
+  end
+end
+
 module ExpenseTracker
     class API < Sinatra::Base
+        def initialize(app = nil, ledger: Ledger.new)
+            @ledger = ledger
+            super(app)
+        end
+
         post '/expenses' do
             JSON.generate('expense_id' => 42)
         end
