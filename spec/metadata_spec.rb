4ba40fcd2c578609551054133a81ba063fb39b5d
Ryu Sato <ryu@weseek.co.jp>
2025-06-29 11:51:59 +0000

    feat: enhance metadata_spec with outer_group and fast metadata usage

--- spec/metadata_spec.rb
+++ spec/metadata_spec.rb
@@ -1,7 +1,13 @@
 require 'pp'
 
-RSpec.describe Hash do
-    it 'is used by RSpec for metadata' do |example|
+RSpec.describe Hash, :outer_group do
+    it 'is used by RSpec for metadata', :fast do |example|
         pp example.metadata
+    end
+
+    context 'on a nested group' do
+        it 'is also inherited' do |example|
+            pp example.metadata
+        end
     end
 end
